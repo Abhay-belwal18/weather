0203b8d5af22e2940f18f02d4cbe7781afeafd62
Abhay Belwal <149303097+Abhay-belwal18@users.noreply.github.com>
2024-07-07 20:13:01 +0530

    Update script.js

--- weather/script.js
+++ weather/script.js
@@ -12,22 +12,22 @@ function convertion(val) {
 
 btn.addEventListener('click', function () 
 {
-    fetch('https://api.openweathermap.org/data/3.0/onecall?q=' + inputvalue.value + '&appid=' + apik)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputvalue.value+ '&appid=' + apik)
         .then(res => res.json())
 
         .then(data =>
     {
         var nameval = data['name']
         var descrip = data['weather']['0']['description']
-        var tempreature = data['main']['temp']
+        var temperature = data['main']['temp']
         var windspeed = data['wind']['speed']
 
-        city.innerHTML = `Weather of <span>${nameval}<span>`
+        city.innerHTML = `Weather : <span>${nameval}<span>`
         temp.innerHTML = `Temperature: <span>${convertion(temperature)} C</span>`
-        description.innerHTML = `sky Conditions: ,span>${descrip}<span>`
+        description.innerHTML = `sky Conditions: span>${descrip}<span>`
         wind.innerHTML = `Wind Speed: <span>${windspeed} km/h<span>`
     })
 
 
-.catch(err  = > alert('You entered wrong city name'))
+.catch(err  => alert('You entered wrong city name'))
 })
