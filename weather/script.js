26905a7ed7abfc8bc1ca5a5ee29cc69823d473b8
Abhay Belwal <149303097+Abhay-belwal18@users.noreply.github.com>
2024-07-07 19:13:03 +0530

    Add files via upload

---
+++ weather/script.js
@@ -0,0 +1,33 @@
+var inputvalue = document.querySelector('#cityinput');
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput');
+var descrip = document.querySelector('#description');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+
+apik = "39c335c50668c1cafac29b7f23e92ce2"
+function convertion(val) {
+    return (val - 273).toFixed(3)
+}
+
+btn.addEventListener('click', function () 
+{
+    fetch('https://api.openweathermap.org/data/3.0/onecall?q=' + inputvalue.value + '&appid=' + apik)
+        .then(res => res.json())
+
+        .then(data =>
+    {
+        var nameval = data['name']
+        var descrip = data['weather']['0']['description']
+        var tempreature = data['main']['temp']
+        var windspeed = data['wind']['speed']
+
+        city.innerHTML = `Weather of <span>${nameval}<span>`
+        temp.innerHTML = `Temperature: <span>${convertion(temperature)} C</span>`
+        description.innerHTML = `sky Conditions: ,span>${descrip}<span>`
+        wind.innerHTML = `Wind Speed: <span>${windspeed} km/h<span>`
+    })
+
+
+.catch(err  = > alert('You entered wrong city name'))
+})
